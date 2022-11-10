11715ee4b76013d172fabc7788ca05ebb05bb979
Michael Zabaneh <michael.zabaneh@sjsu.edu>
2022-11-09 21:38:42 -0800

    Re repush of HW to regrade

--- sort.py
+++ sort.py
@@ -2,12 +2,12 @@ def sort_dictionary(dict):
     dictlist=list(dict.items())
     l=len(dictlist)
     finalListofTup = []
-	for i in range(l - 1):
-		for j in range(i + 1, l):
-			if (dictlist[i][1] > dictlist[j][1]):
-				t = dictlist[i]
-				dictlist[i] = dictlist[j]
-				dictlist[j]=t
-		for a, b, in dictlist:
-			finalListofTup.append((a, b[0]))
-		return finalListofTup
+    for i in range(l - 1):
+        for j in range(i + 1, l):
+            if (dictlist[i][1] > dictlist[j][1]):
+                t = dictlist[i]
+                dictlist[i] = dictlist[j]
+                dictlist[j]=t
+        for a, b, in dictlist:
+            finalListofTup.append((a, b[0]))
+        return finalListofTup
